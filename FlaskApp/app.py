95161e85ec345404cf6613faad122c6efab61a3c
kylinZhuo <kylin.zhuo@gmail.com>
2018-03-22 13:36:19 -0700

    03/22/2018 13:36

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect
 from flask.ext.wtf import Form
 # from flask.ext.bootstrap import Bootstrap
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, validators
 from wtforms.validators import Required
 
 from functions import *
@@ -12,6 +12,13 @@ app.config['SECRET_KEY'] = 'Set key'
 class NameForm(Form):
     name = StringField('Player name', validators=[Required()])
     year = StringField('Year', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class RivaryForm(Form):
+    name1 = StringField('Player 1', validators=[Required()])
+    name2 = StringField('Player 2', validators=[Required()])
+    from_year = StringField('From', validators=[validators.Length(min=0, max=4)])
+    to_year = StringField('To', validators=[validators.Length(min=0, max=4)])
     submit = SubmitField('Submit')
 
 def process(name, year):
@@ -31,6 +38,7 @@ def facts():
     name = None
     year = None
     data = None
+    keys = None
     nameForm = NameForm()
 
     if nameForm.validate_on_submit():
@@ -40,17 +48,34 @@ def facts():
         nameForm.year.data = ''
         flash('Player: ' + str(name))
         flash('Year: ' + str(year))
-        data = player_info(name, year)
+        keys, data = player_info(name, year)
 
     return render_template('facts.html',
     name=name,
     year=year,
     form=nameForm,
+    keys=keys,
     data=data)
 
-@app.route('/head2head')
+@app.route('/head2head', methods=['GET', 'POST'])
 def head2head():
-    return render_template('head2head.html')
+    name1, name2 = None, None
+    from_year, to_year = None, None
+    result = None
+    rivaryForm = RivaryForm()
+
+    if rivaryForm.validate_on_submit():
+        name1 = rivaryForm.name1.data
+        name2 = rivaryForm.name2.data
+        from_year = rivaryForm.from_year.data
+        to_year = rivaryForm.to_year.data
+        # rivaryForm.name1.data = ''
+        # rivaryForm.name2.data = ''
+        flash('Player1: ' + str(name1))
+        flash('Player2: ' + str(name2))
+        result = rivary(name1, name2, from_year, to_year)
+
+    return render_template('head2head.html', result=result, form=rivaryForm)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- FlaskApp/functions.py
+++ FlaskApp/functions.py
@@ -5,7 +5,8 @@ import numpy as np
 from collections import Counter
 
 data_path = '../tennis_atp-master/'
-
+MIN_YEAR = 1968
+MAX_YEAR = 2018
 
 def player_info(player, year, with_davis_cup = True):
 
@@ -25,7 +26,7 @@ def player_info(player, year, with_davis_cup = True):
     ace_rate_overall = (np.sum(winning_slice.w_ace) + np.sum(losing_slice.l_ace)) / \
      (np.sum(winning_slice['w_svpt']) + np.sum(losing_slice['l_svpt']))
 
-    res = {
+    data = {
     'player': player,
     'year': year,
     'n_winning': n_winning,
@@ -35,4 +36,46 @@ def player_info(player, year, with_davis_cup = True):
     'ace_rate_overall': ace_rate_overall
     }
 
+    keys = ['player', 'year', 'n_winning', 'n_losing',
+    'ace_rate_winning', 'ace_rate_losing', 'ace_rate_overall']
+
+    return keys, data
+
+# Extract the rivary data of two players
+def rivary(p1, p2, from_year, to_year):
+
+    print p1, p2, from_year, to_year
+
+    if not from_year or len(from_year) == 0:
+        from_year = MIN_YEAR
+    if not to_year or len(to_year) == 0:
+        to_year = MAX_YEAR
+
+    from_year = int(from_year)
+    to_year = int(to_year)
+
+    assert(from_year <= to_year)
+
+    wins, loses = 0, 0
+    matches = []
+
+    for year in range(from_year, to_year+1):
+        df = pd.read_csv(data_path + "atp_matches_%s.csv" % year)
+        df = df[((df['winner_name'] == p1) & (df['loser_name'] == p2)) | ((df['loser_name'] == p1) & (df['winner_name'] == p2))]
+
+        for i in range(len(df)):
+            matches.append(dict(df.iloc[i]))
+
+        head_2_heads = Counter(zip(df["winner_name"], df["loser_name"]))
+        wins += head_2_heads[(p1, p2)]
+        loses += head_2_heads[(p2, p1)]
+
+    matches.sort(key=lambda x:x['tourney_date'], reverse=True)
+
+    res = {
+    'p1': p1,
+    'p2': p2,
+    'h2h': [wins, loses],
+    'matches': matches
+    }
     return res
