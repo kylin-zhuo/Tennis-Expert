4dd3fc80e503dc8bc10752f44fc053fbf7f0705d
kylinZhuo <kylin.zhuo@gmail.com>
2018-03-24 20:53:09 -0700

    03/24/2018 20:53

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect
 from flask.ext.wtf import Form
 # from flask.ext.bootstrap import Bootstrap
-from wtforms import StringField, SubmitField, validators
+from wtforms import StringField, SubmitField, BooleanField, validators
 from wtforms.validators import Required
 
 from functions import *
@@ -11,7 +11,8 @@ app.config['SECRET_KEY'] = 'Set key'
 
 class NameForm(Form):
     name = StringField('Player name', validators=[Required()])
-    year = StringField('Year', validators=[Required()])
+    year = StringField('Year', validators=[validators.Length(min=0, max=4)])
+    davis_cup = BooleanField('Include Davis Cup?', default="checked")
     submit = SubmitField('Submit')
 
 class RivaryForm(Form):
@@ -39,16 +40,18 @@ def facts():
     year = None
     data = None
     keys = None
+    davis_cup = False
     nameForm = NameForm()
 
     if nameForm.validate_on_submit():
         name = nameForm.name.data
         year = nameForm.year.data
+        davis_cup = nameForm.davis_cup.data
         nameForm.name.data = ''
         nameForm.year.data = ''
         flash('Player: ' + str(name))
         flash('Year: ' + str(year))
-        keys, data = player_info(name, year)
+        keys, data = player_info(name, year, davis_cup)
 
     return render_template('facts.html',
     name=name,
@@ -63,6 +66,11 @@ def head2head():
     from_year, to_year = None, None
     result = None
     rivaryForm = RivaryForm()
+    chart = None
+    series = None
+    title = None
+    xAxis = None
+    yAxis = None
 
     if rivaryForm.validate_on_submit():
         name1 = rivaryForm.name1.data
@@ -75,7 +83,17 @@ def head2head():
         flash('Player2: ' + str(name2))
         result = rivary(name1, name2, from_year, to_year)
 
-    return render_template('head2head.html', result=result, form=rivaryForm)
+        ## the following is for testing
+        chart = {"renderTo": 'chartID', "type": 'bar', "height": 450,}
+        series = [
+        {"name": name1, "data": [result['h2h'][0], result['h2h_grass'][0], result['h2h_hard'][0], result['h2h_clay'][0]]}, 
+        {"name": name2, "data": [result['h2h'][1], result['h2h_grass'][1], result['h2h_hard'][1], result['h2h_clay'][1]]}]
+        title = {"text": 'Head-to-head between ' + name1 + ' and ' + name2}
+        xAxis = {"categories": ['All', 'Grass', 'Hard', 'Clay']}
+        yAxis = {"title": {"text": 'Wins'}}
+
+    return render_template('head2head.html', result=result, form=rivaryForm, 
+         chartID='chartID', chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- FlaskApp/functions.py
+++ FlaskApp/functions.py
@@ -2,44 +2,61 @@ import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
 # import seaborn as sns
-from collections import Counter
+from collections import Counter, defaultdict
 
-data_path = '../tennis_atp-master/'
+data_path = "../tennis_atp-master/"
 MIN_YEAR = 1968
 MAX_YEAR = 2018
 
+
 def player_info(player, year, with_davis_cup = True):
 
-    df = pd.read_csv(data_path + "atp_matches_%s.csv" % year)
-    if not with_davis_cup:
-        df = df[df.tourney_name.str.contains("Davis") == False]
+    if year:
+        from_year = int(year)
+        to_year = int(year)
+    else:
+        from_year = MIN_YEAR
+        to_year = MAX_YEAR
 
-    assert(len(df['tourney_id'].unique()) == len(df['tourney_name'].unique()))
+    res = dict()
 
-    winning_slice = df[df["winner_name"] == player]
-    losing_slice = df[df["loser_name"] == player]
+    for year in range(from_year, to_year+1):
 
-    n_winning, n_losing = len(winning_slice), len(losing_slice)
+        df = pd.read_csv(data_path + "atp_matches_%s.csv" % str(year))
+        if not with_davis_cup:
+            df = df[df.tourney_name.str.contains("Davis") == False]
 
-    ace_rate_winning = np.sum(winning_slice.w_ace) / np.sum(winning_slice['w_svpt'])
-    ace_rate_losing = np.sum(losing_slice.l_ace) / np.sum(losing_slice['l_svpt'])
-    ace_rate_overall = (np.sum(winning_slice.w_ace) + np.sum(losing_slice.l_ace)) / \
-     (np.sum(winning_slice['w_svpt']) + np.sum(losing_slice['l_svpt']))
+        # assert(len(df['tourney_id'].unique()) == len(df['tourney_name'].unique()))
 
-    data = {
-    'player': player,
-    'year': year,
-    'n_winning': n_winning,
-    'n_losing': n_losing,
-    'ace_rate_winning': ace_rate_winning,
-    'ace_rate_losing': ace_rate_losing,
-    'ace_rate_overall': ace_rate_overall
-    }
+        winning_slice = df[df["winner_name"] == player]
+        losing_slice = df[df["loser_name"] == player]
+
+        n_winning, n_losing = len(winning_slice), len(losing_slice)
+
+        if n_winning and n_losing:
+
+            ace_rate_winning = np.sum(winning_slice.w_ace) / np.sum(winning_slice['w_svpt'])
+            ace_rate_losing = np.sum(losing_slice.l_ace) / np.sum(losing_slice['l_svpt'])
+            ace_rate_overall = (np.sum(winning_slice.w_ace) + np.sum(losing_slice.l_ace)) / \
+             (np.sum(winning_slice['w_svpt']) + np.sum(losing_slice['l_svpt']))
+
+            data = {
+            'player': player,
+            'year': year,
+            'n_winning': n_winning,
+            'n_losing': n_losing,
+            'ace_rate_winning': ace_rate_winning,
+            'ace_rate_losing': ace_rate_losing,
+            'ace_rate_overall': ace_rate_overall
+            }
+
+            res[year] = data
 
     keys = ['player', 'year', 'n_winning', 'n_losing',
     'ace_rate_winning', 'ace_rate_losing', 'ace_rate_overall']
 
-    return keys, data
+    return keys, res
+
 
 # Extract the rivary data of two players
 def rivary(p1, p2, from_year, to_year):
@@ -57,18 +74,35 @@ def rivary(p1, p2, from_year, to_year):
     assert(from_year <= to_year)
 
     wins, loses = 0, 0
+    wins_grass, loses_grass = 0, 0
+    wins_hard, loses_hard = 0, 0
+    wins_clay, loses_clay = 0, 0
     matches = []
 
     for year in range(from_year, to_year+1):
         df = pd.read_csv(data_path + "atp_matches_%s.csv" % year)
         df = df[((df['winner_name'] == p1) & (df['loser_name'] == p2)) | ((df['loser_name'] == p1) & (df['winner_name'] == p2))]
 
+        df_grass = df[df['surface'] == 'Grass']
+        df_hard = df[df['surface'] == 'Hard']
+        df_clay = df[df['surface'] == 'Clay']
+
         for i in range(len(df)):
             matches.append(dict(df.iloc[i]))
 
         head_2_heads = Counter(zip(df["winner_name"], df["loser_name"]))
+        head_2_heads_grass = Counter(zip(df_grass["winner_name"], df_grass["loser_name"]))
+        head_2_heads_hard = Counter(zip(df_hard["winner_name"], df_hard["loser_name"]))
+        head_2_heads_clay = Counter(zip(df_clay["winner_name"], df_clay["loser_name"]))
+
         wins += head_2_heads[(p1, p2)]
         loses += head_2_heads[(p2, p1)]
+        wins_grass += head_2_heads_grass[(p1, p2)]
+        loses_grass += head_2_heads_grass[(p2, p1)]
+        wins_hard += head_2_heads_hard[(p1, p2)]
+        loses_hard += head_2_heads_hard[(p2, p1)]
+        wins_clay += head_2_heads_clay[(p1, p2)]
+        loses_clay += head_2_heads_clay[(p2, p1)]        
 
     matches.sort(key=lambda x:x['tourney_date'], reverse=True)
 
@@ -76,6 +110,9 @@ def rivary(p1, p2, from_year, to_year):
     'p1': p1,
     'p2': p2,
     'h2h': [wins, loses],
+    'h2h_grass': [wins_grass, loses_grass],
+    'h2h_hard': [wins_hard, loses_hard],
+    'h2h_clay': [wins_clay, loses_clay],
     'matches': matches
     }
     return res
